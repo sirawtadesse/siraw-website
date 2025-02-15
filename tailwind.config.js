927e584b55e898f476955e4c5f48ef272750b2b8
sirawtadesse <128708396+sirawtadesse@users.noreply.github.com>
2025-02-15 10:41:43 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  eslint: {
+    // Add the rules you want to disable
+    rules: {
+      'react/jsx-no-undef': 'off',
+    },
+  },
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
