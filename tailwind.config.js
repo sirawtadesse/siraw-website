522b75e6a968f0e1768ee43ef7cf2efe93f43649
Siraw Tadesse <128708396+sirawtadesse@users.noreply.github.com>
2025-04-11 13:26:44 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   eslint: {
-    // Add the rules you want to disable
     rules: {
       'react/jsx-no-undef': 'off',
     },
@@ -12,7 +11,17 @@ module.exports = {
   ],
   darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        fadeInUp: {
+          '0%': { opacity: '0', transform: 'translate3d(0, 40px, 0)' },
+          '100%': { opacity: '1', transform: 'translate3d(0, 0, 0)' },
+        },
+      },
+      animation: {
+        fadeInUp: 'fadeInUp 0.8s ease-out forwards',
+      },
+    },
   },
   plugins: [],
 }
